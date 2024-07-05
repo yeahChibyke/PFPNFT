fb9c29ac27098476cd439d0ac0b0437e47cf456d
yeahChibyke <chibykepride@gmail.com>
2024-07-06 00:03:00 +0100

    more tests? let's have it

--- test/TestPfpNft.t.sol
+++ test/TestPfpNft.t.sol
@@ -8,6 +8,8 @@ import {PfpNft} from "../src/PfpNft.sol";
 contract TestPfpNft is Test {
     DeployPfpNft public deployPfpNft;
     PfpNft public pfpNft;
+    address public Chibyke = makeAddr("Chibyke");
+    string public constant PFP_URI = "ipfs://QmPP2EMAF1ask7NQYHuw3hfgUJjJDSQdbrnAhe8Fxp3b4G";
 
     function setUp() public {
         deployPfpNft = new DeployPfpNft();
@@ -23,5 +25,20 @@ contract TestPfpNft is Test {
 
         assert(hashedExpectedName == hashedActualName);
         assert(hashedExpectedSymbol == hashedActualSymbol);
+    }
+
+    function testTokenUriIsCorrect() public pure {
+        bytes32 hashedExpectedTokenUri = keccak256(abi.encodePacked("ipfs://QmPP2EMAF1ask7NQYHuw3hfgUJjJDSQdbrnAhe8Fxp3b4G"));
+        bytes32 hashedActualTokenUri = keccak256(abi.encodePacked(PFP_URI));
+
+        assert(hashedExpectedTokenUri == hashedActualTokenUri);
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(Chibyke);
+        pfpNft.mintNft(PFP_URI);
+
+        assert(pfpNft.balanceOf(Chibyke) == 1);
+        assert(pfpNft.ownerOf(0) == Chibyke);
     }
 }
